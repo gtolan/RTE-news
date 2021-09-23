850bb0b2f121ded57db6b6da8af5ff66a9e0ba5b
GTolan <tolangerard@gmail.com>
2021-09-23 15:42:28 +0100

    seperate articles with and without images

--- src/Components/ArticleList.js
+++ src/Components/ArticleList.js
@@ -1,35 +1,75 @@
 import { useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import useArticles from './useArticles';
+import '../styles/Articles.scss';
+
+
+
 
 const ArticleList = () => {
     
     const { fetchArticles } = useArticles();
-    const articles = useSelector(state => {
-            console.log(state.art, "AT")
-        
-       return state.art.articlesWithImages});
+    const articlesWithImages = useSelector(state => state.art.articlesWithImages);
+    const articlesWithoutImages = useSelector(state => state.art.articlesWithoutImages);
+
 
     useEffect(() => {
         console.log('use eff fetch')
         fetchArticles()
     }, [])
 
-    if(articles.length === 0) return null;
-
-    return (
-        <>
-        {articles.map(article => (
-            <article>
-                <div className="card-image">
-                    <img src={article.urlToImage} alt={article.title} />
-                </div>    
+    const WithImages = () =>{
+        return (
+            <section className="articles-with-images">
+                {articlesWithImages.map((article, id) => {
+                return (id !== 0) ? 
+                (<article key={article.title}>
+                    <div className="card-image">
+                        <img src={article.urlToImage} alt={article.title} />
+                    </div>    
+                    <div className="article-body">
+                        {article.title}
+                    </div>
+                </article>) :
+                null
+                
+            }
+            )}
+            </section>)
+    }
+    const WithoutImages = () => {
+        return (articlesWithoutImages.map(article => (
+            <article className="without-images"> 
                 <div className="article-body">
                     {article.title}
                 </div>
             </article>
-        ))
-        }
+        )))
+    }
+
+    const HeadlineArticle = () => {
+        return (
+                        <article>
+                <div className="card-image">
+                    <img src={articlesWithImages[0].urlToImage} alt={articlesWithImages[0].title} />
+                </div>    
+                <div className="article-body">
+                    {articlesWithImages[0].title}
+                </div>
+            </article>
+        )
+    }
+
+    if(articlesWithImages.length === 0) return null;
+
+    return (
+        <>
+        <div className="headline-article">
+            <HeadlineArticle/>
+            <WithoutImages />
+        </div>
+        <WithImages />
+        
         </>
     )
 }
